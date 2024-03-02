cef9e940837e4c0d43c84e009de6ea417618c360
Akash <123761006+Akash19304@users.noreply.github.com>
2024-03-02 23:11:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ socketio = SocketIO(app)
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
+index=0
 cap = cv2.VideoCapture(index, cv2.CAP_V4L)
 last_eye_detected_time = time.time()
 cheating_count = 0
