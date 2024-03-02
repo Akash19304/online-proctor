23a85935b8b429108ad562e919722146421db70e
Akash <123761006+Akash19304@users.noreply.github.com>
2024-03-02 23:18:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fronta
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 last_eye_detected_time = time.time()
 cheating_count = 0
 
